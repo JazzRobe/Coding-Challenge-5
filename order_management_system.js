4b1d940fa34fc5c8eab723c90dd57e6401a20ccb
Jazzmyne Roberson <jroberson0927@gmail.com>
2024-09-19 11:58:44 -0400

    Initialize orders array

--- order_management_system.js
+++ order_management_system.js
@@ -5,4 +5,18 @@ const inventory = [
     {name: "Latte", price: 4, quantity: 5},
     {name: "Cappucino", price: 4, quantity: 6},
     {name: "Mocha", price: 5, quantity: 4}
-]
+];
+
+
+// TASK 2: Create an Orders Array of Order Objects
+
+let orders = [
+    {customerName: null}, // nulls for empty values
+    {items: [
+        {name: null},
+        {quantity: null}
+    ]},
+    {status: "Pending"} // default value pending, when completed it will get changed
+];
+
+// console.log(orders); // feel free to test any commented console.logs :)
